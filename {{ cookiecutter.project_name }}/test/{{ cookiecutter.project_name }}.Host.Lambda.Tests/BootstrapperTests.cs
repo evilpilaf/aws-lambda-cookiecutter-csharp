452bf2e6104a302149c2d6014d02cf6696097b98
mauricio-ddelc <mauriciod73@gmail.com>
2019-02-06 18:04:46 +0100

    Add tests for the bootstrapper

---
+++ {{ cookiecutter.project_name }}/test/{{ cookiecutter.project_name }}.Host.Lambda.Tests/BootstrapperTests.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Coolblue.Serilog.Sinks.InMemory;
+
+using {{ cookiecutter.project_name }}.Core;
+using {{ cookiecutter.project_name }}.Core.Ports;
+using {{ cookiecutter.project_name }}.Core.ValueTypes;
+
+using FluentAssertions;
+using Moq;
+using SimpleInjector;
+using Xunit;
+
+{% if cookiecutter.use_kms =="YES" -%}
+using SecretManagement.Adapter.InMemory;
+using SecretManagement.Adapter.Kms;
+{% endif -%}
+
+
+namespace {{ cookiecutter.project_name }}.Host.Lambda.Tests
+{
+    public sealed class BootstrapperTests
+    {
+        private LambdaSettings TestSettings(ApplicationEnvironment environment) =>
+            new LambdaSettings {
+                Environment = environment
+            };
+
+        {% if cookiecutter.use_kms =="YES" -%}
+        [Fact]
+        public void GivenADevelopmentEnvironment_WhenContainerResolvesISecretManagementService_GetsInstanceOfInMemoryService()
+        {
+            var sut = CreateSut(TestSettings(ApplicationEnvironment.Development));
+            var instance = sut.GetInstance<ISecretManagementService>();
+
+            instance.Should().BeOfType<InMemorySecretManagementService>();
+        }
+
+        [Theory]
+        [InlineData(ApplicationEnvironment.Testing)]
+        [InlineData(ApplicationEnvironment.Acceptance)]
+        [InlineData(ApplicationEnvironment.Production)]
+        public void GivenTAPEnvironment_WhenContainerResolvesISecretManagementService_GetsInstanceOfKmsService(ApplicationEnvironment environment)
+        {
+            var sut = CreateSut(TestSettings(environment));
+            var instance = sut.GetInstance<ISecretManagementService>();
+
+            instance.Should().BeOfType<KmsSecretManagementService>();
+        }
+        {% endif -%}
+
+        [Theory]
+        [InlineData(ApplicationEnvironment.Development)]
+        [InlineData(ApplicationEnvironment.Testing)]
+        [InlineData(ApplicationEnvironment.Acceptance)]
+        [InlineData(ApplicationEnvironment.Production)]
+        public void ContainerResolvesUseCase(ApplicationEnvironment environment)
+        {
+            var sut = CreateSut(TestSettings(environment));
+
+            Action func = () => sut.GetInstance<{{ cookiecutter.project_name }}UseCase>();
+
+            func.Should().NotThrow();
+        }
+
+        private Container CreateSut(LambdaSettings settings) => Bootstrapper.CreateDefaultApplication(settings);
+    }
+}
